73fd75ac43f38f124780485913b73724113f7a60
cryingdev <33308581+cryingdev@users.noreply.github.com>
2025-11-13 16:34:29 +0900

    Initial Commit

---
+++ PhotoSearch/PhotoSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoSearchApp.swift
+//  PhotoSearch
+//
+//  Created by G.K.LEE on 11/13/25.
+//
+
+import SwiftUI
+
+@main
+struct PhotoSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
